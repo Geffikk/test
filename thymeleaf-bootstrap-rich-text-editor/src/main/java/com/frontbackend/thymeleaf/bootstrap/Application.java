fa97bf731ba41cd5bcb18195b51928a5282dc6bd
Maros Geffert <maros.geffert@gmail.com>
2020-11-18 13:08:42 +0100

    test2

--- thymeleaf-bootstrap-rich-text-editor/src/main/java/com/frontbackend/thymeleaf/bootstrap/Application.java
+++ thymeleaf-bootstrap-rich-text-editor/src/main/java/com/frontbackend/thymeleaf/bootstrap/Application.java
@@ -8,6 +8,7 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("test1");
+        System.out.println("test2");
         SpringApplication.run(Application.class, args);
     }
 }
