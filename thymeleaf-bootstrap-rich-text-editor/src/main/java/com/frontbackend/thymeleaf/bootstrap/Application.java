262e3aa71c78864e5f46d0128c50a36c001205b5
Maros Geffert <maros.geffert@gmail.com>
2020-11-18 13:08:42 +0100

    Revert "test2"
    This reverts commit fa97bf731ba41cd5bcb18195b51928a5282dc6bd.

--- thymeleaf-bootstrap-rich-text-editor/src/main/java/com/frontbackend/thymeleaf/bootstrap/Application.java
+++ thymeleaf-bootstrap-rich-text-editor/src/main/java/com/frontbackend/thymeleaf/bootstrap/Application.java
@@ -8,7 +8,6 @@ public class Application {
 
     public static void main(String[] args) {
         System.out.println("test1");
-        System.out.println("test2");
         SpringApplication.run(Application.class, args);
     }
 }
